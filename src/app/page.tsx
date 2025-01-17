322331a307e2577983cdbe2fcabbd312c81440e1
amirrowll <amirhosseinmhmd8384@gmail.com>
2025-01-17 23:27:20 +0330

    map is picture & without API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Container from "@/components/container/Container";
 import Features from "@/components/features/Features";
 import Header from "@/components/header/Header";
 import Introduction from "@/components/introduction/Introduction";
+import Map from "@/components/map/Map";
 import Reviews from "@/components/reviews/Reviews";
 import Services from "@/components/services/Services";
 
@@ -16,6 +17,7 @@ export default function Home() {
         <Introduction />
         <Reviews />
         <CommonQuestion />
+        <Map />
       </Container>
     </div>
   );
---
+++ src/components/map/Map.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Map() {
+  return (
+    <div className=''>
+        <img className='mt-[-10px] mr-[-20px] max-w-max' src="/imageLanding/Location-Map.svg" alt="img" />
+    </div>
+  )
+}
